0b73f53686f1495f7751c16ee177bc1050cd91e6
Mateus Santos <mateus.pereira99@hotmail.com>
2019-09-02 19:26:55 -0300

    Input de texto

--- src/Components/Inputs/TextInput.js
+++ src/Components/Inputs/TextInput.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Item, Input, Label } from 'native-base';
+import { Item, Input, Label, View } from 'native-base';
 
 const TextInput = ( { label, textColor, type } ) => {
     const styles = StyleSheet.create({
+        borderInput: {
+            borderColor: textColor,
+            borderWidth: 1,
+            borderRadius: 30,
+            height: 40,
+            marginTop: 20
+        },
         item: {
-            borderBottomColor: textColor,
-            borderBottomWidth: 2,
-            marginTop: 16
+            borderBottomWidth: 0,
+            marginTop: -15,
+            marginLeft: 16
         },
         label: {
             color: textColor
         },
         input: {
             fontSize: 16,
-            color: "#3a3a3a"
+            color: "#3a3a3a",
+            marginTop: 3
         }
     });
     return(
-        <Item floatingLabel style={styles.item}>
-            <Label style={styles.label}>{label}</Label>
-            {type === "text" &&
-                <Input style={styles.input}/>
-            }
-            {type === "password" &&
-                <Input style={styles.input} secureTextEntry={true}/>
-            }
-        </Item>
+        <View style={styles.borderInput}>
+            <Item floatingLabel style={styles.item}>
+                <Label style={styles.label}>{label}</Label>
+                {type === "text" &&
+                    <Input style={styles.input}/>
+                }
+                {type === "password" &&
+                    <Input style={styles.input} secureTextEntry={true}/>
+                }
+            </Item>
+        </View>
     )
 }
 
